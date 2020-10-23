8081eb0408a1023e08ee9dd4ca90d5a9441416bd
tomoakley <tomoakley@MiMAC002.local>
2020-10-23 17:14:33 +0100

    oops missed a warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import fetch from "node-fetch";
 import styled from "styled-components";
 
