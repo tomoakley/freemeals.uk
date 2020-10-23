b6905fcfec19e7010e5bb44d8bacf53684262bfb
KingMob <john@onethreethree.net>
2020-10-23 18:20:28 +0100

    add map and markers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import L from "leaflet";
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import fetch from "node-fetch";
 import styled from "styled-components";
 
@@ -15,6 +16,23 @@ const Header = styled.div`
 `;
 
 const Heading = styled.h1``;
+
+const Option = styled.h3`
+  display: inline;
+  padding: 10px;
+  border-bottom: solid black 1px;
+  cursor: pointer;
+  &:hover {
+  background: blue;
+  color: white;
+  }
+  ${(props) =>
+  props.isSelected &&
+  `
+  background: blue;
+  color: white;
+  `}
+`;
 
 const List = styled.ul`
   list-style: none;
@@ -50,11 +68,13 @@ const SelectedPane = styled.div`
 
 function App() {
   const mapRef = useRef(null);
+  const [mode, setMode] = useState("list");
   const [data, setData] = useState([]);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [markerPositions, setMarkerPositions] = useState([
     { lat: 53.937, lng: -3.274 },
   ]);
+  const [markers, setMarkers] = useState();
 
   useEffect(() => {
     fetch(".netlify/functions/providers/")
@@ -64,6 +84,10 @@ function App() {
         console.log(data);
       });
   }, []);
+
+  useEffect(()=>{
+    initMarkers();
+  },[data])
 
   /* useEffect(() => {
     mapRef.current = L.map("map", {
@@ -79,11 +103,7 @@ function App() {
   }, [markerPositions]);
 
   const markerRef = useRef(null);
-  const customIcon = L.icon({
-    iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
-    iconSize: [35, 46],
-    iconAnchor: [17, 46],
-  });
+  
 
   useEffect(() => {
     markerPositions.forEach((position) => {
@@ -96,6 +116,41 @@ function App() {
       }
     });
   }, [markerPositions, customIcon]); */
+
+  const initMarkers = async () => {
+    const customIcon = L.icon({
+      iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
+      iconSize: [35, 46],
+      iconAnchor: [17, 46],
+    });
+
+     if(data.length)
+      setMarkers(data.map((provider, i)=>{
+
+        if(!provider.latitude){
+          provider.latitude = 56 - (i*0.05);
+          provider.longitude = -5 + (i*0.05);
+        }
+
+        let position = [provider.latitude,provider.longitude];
+        
+        return (
+              <Marker
+                key={i}
+                position={position}
+                icon={customIcon}
+              >
+                <Popup>
+                  <Block>{provider["provider name"]}</Block>
+                  <Block>{provider["provider address 1"]}</Block>
+                  <Block>{provider["provider url"]}</Block>
+                </Popup>
+              </Marker>
+            )
+          }
+        )
+      )
+  }
 
   const handleProviderClick = (i) => {
     setSelectedIndex(i);
@@ -110,13 +165,23 @@ function App() {
   return (
     <>
       <Header>
-9       <Heading>Freemeals.uk</Heading>
+        <Heading>Freemeals.uk</Heading>
         <span>
           A collated list of venues offering free meals to UK school children
           during the half terms holidays
         </span>
       </Header>
-      <Container>
+      <div className="mode-select">
+        <Option isSelected={mode=="list"}
+        onClick={()=>setMode("list")}  
+        >List</Option>
+        <Option isSelected={mode=="map"}
+        onClick={()=>setMode("map")}        
+        >Map</Option>
+      </div>
+      {
+        mode=="list"?
+        <Container>
         <List>
           {data.length ?
             data.map((provider, i) => (
@@ -157,6 +222,20 @@ function App() {
           <span>Loading</span>
         )}
       </Container>
+        :
+        <div style={{display: "block", height:"100%"}}>
+        <Map center={[53.937, -3.274]} 
+            zoom={6}
+            className="leaflet-map"
+        >
+          <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+          {markers}
+        </Map>
+        </div>
+      }
     </>
   );
 }
