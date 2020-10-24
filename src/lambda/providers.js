f815adc3404c71e44abf112ef2da2936ed007340
Kurt Culley <kurt.culley@gmail.com>
2020-10-24 18:05:48 +0100

    feat: use @tomoakley's sweet geo lookup code to sort results server-side :)

--- src/lambda/providers.js
+++ src/lambda/providers.js
@@ -1,35 +1,58 @@
 import GSheetReader from "g-sheets-api";
+import sphereKnn from "sphere-knn";
 
 export async function handler(event, context) {
   try {
-    const { location } = event.queryStringParameters
-    console.log(location)
+    const { location, coords } = event.queryStringParameters;
+    console.log(location);
     const data = await new Promise((resolve, reject) => {
       GSheetReader(
         {
           sheetId: "1OaRn7UHsFpFLOfTeiUnIBr7ofjcemBEvf_gl5b1PoTY",
           sheetNumber: 5,
-          returnAllResults: location === 'All' ? true : false,
+          returnAllResults: location === "All" ? true : false,
           filter: {
-            'provider town/city': location
+            "provider town/city": location
           }
         },
-        (results) => {
-          resolve(results);
+        results => {
+          if (coords != null) {
+            const [latitude, longitude] = coords.split(",");
+
+            const resultsWithCoords = results.map(provider => {
+              return {
+                ...provider,
+                latitude: Number(provider.latitude),
+                longitude: Number(provider.longitude)
+              };
+            });
+
+            const geolookupData = sphereKnn(resultsWithCoords);
+            const geolocatedResults = geolookupData(
+              latitude,
+              longitude,
+              10,
+              15000
+            );
+            
+            resolve(geolocatedResults);
+          } else {
+            resolve(results);
+          }
         },
-        (error) => {
+        error => {
           reject(error);
         }
       );
     });
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
     };
   }
 }
