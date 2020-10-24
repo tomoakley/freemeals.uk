fd940d250ce7aba9436f034166463f6842e97c8b
marktdodds <marktdodds@gmail.com>
2020-10-24 20:55:25 +0200

    Enabling service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import GeoProvider from "./components/GeoProvider";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./services/serviceWorker.service";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +14,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.registerServiceWorker();
