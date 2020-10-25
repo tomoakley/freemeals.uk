6d213d4fc36a048fc8b8d66e7851bf812b35ffdb
tomoakley <tomoakley@MiMAC002.local>
2020-10-25 20:19:43 +0000

    fix scrolling of locationfilter list

--- src/components/LocationFilter/index.js
+++ src/components/LocationFilter/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../AppContext/AppContext";
-import IconSelected from '../../images/icon-selected.svg'
+import IconSelected from "../../images/icon-selected.svg";
 
 function LocationFilter() {
-  const { locations, setSelectedLocation, selectedLocation } = React.useContext(AppContext);
+  const { locations, setSelectedLocation, selectedLocation } = React.useContext(
+    AppContext
+  );
 
   const handleLocationClick = (e, location) => {
     e.preventDefault();
@@ -12,37 +14,47 @@ function LocationFilter() {
   };
 
   return (
-    <LocationFilterContainer>
+    <>
       <strong>Choose location</strong>
-      {!!locations?.length &&
-        locations.sort().slice().map((location) => (
-          <LocationItem key={location}>
-            <Block
-              href="#"
-              onClick={(e) => handleLocationClick(e, location)}
-              isSelected={selectedLocation === location}
-            >
-              {selectedLocation === location && (
-                <SelectedItemIcon src={IconSelected} alt={`Selected location: ${selectedLocation}}`} />
-              )}
-              {location}
-            </Block>
-          </LocationItem>
-        ))}
-    </LocationFilterContainer>
+      <LocationFilterContainer>
+        {!!locations?.length &&
+          locations
+            .sort()
+            .slice()
+            .map((location) => (
+              <LocationItem key={location}>
+                <Block
+                  href="#"
+                  onClick={(e) => handleLocationClick(e, location)}
+                  isSelected={selectedLocation === location}
+                >
+                  {selectedLocation === location && (
+                    <SelectedItemIcon
+                      src={IconSelected}
+                      alt={`Selected location: ${selectedLocation}}`}
+                    />
+                  )}
+                  {location}
+                </Block>
+              </LocationItem>
+            ))}
+      </LocationFilterContainer>
+    </>
   );
 }
 
 const Block = styled.a`
   display: block;
   padding: 1px 0;
-  color: #F5F5F5;
+  color: #f5f5f5;
   text-decoration: none;
   cursor: pointer;
   &:hover {
-    color: #FFFFFF;
+    color: #ffffff;
   }
-  ${({ isSelected }) => isSelected && `
+  ${({ isSelected }) =>
+    isSelected &&
+    `
     font-weight: bold;
   `}
 `;
@@ -52,7 +64,8 @@ const LocationFilterContainer = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
-  overflow-y: scroll;
+  overflow-y: auto;
+  height: calc(100vh - 480px);
 `;
 
 const LocationItem = styled.li`
@@ -61,6 +74,6 @@ const LocationItem = styled.li`
 
 const SelectedItemIcon = styled.img`
   margin-right: 5px;
-`
+`;
 
 export default LocationFilter;
